47c7d497657f64930c8cb05ab13fc5a8d103d10f
Jenney <ren.jenney@gmail.com>
2021-04-28 20:54:48 -0400

    add map controls

--- src/components/tabComponents/Map.tsx
+++ src/components/tabComponents/Map.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {AzureMap, AzureMapsProvider, IAzureMapOptions} from 'react-azure-maps';
-import { AuthenticationType } from 'azure-maps-control';
+import { AzureMap, IAzureMapOptions, IAzureMapControls } from 'react-azure-maps';
+import { AuthenticationType, ControlOptions } from 'azure-maps-control';
 import { mapSubscription } from '../../config';
 
 const option: IAzureMapOptions = {
@@ -8,11 +8,38 @@ const option: IAzureMapOptions = {
         authType: AuthenticationType.subscriptionKey,
         subscriptionKey: mapSubscription
     },
-}
+};
+
+const controls: IAzureMapControls[] = [
+    {
+      controlName: 'StyleControl',
+      controlOptions: { mapStyles: 'all' },
+      options: { position: 'top-right' } as ControlOptions,
+    },
+    {
+      controlName: 'ZoomControl',
+      options: { position: 'top-right' } as ControlOptions,
+    },
+    {
+      controlName: 'CompassControl',
+      controlOptions: { rotationDegreesDelta: 10 },
+      options: { position: 'top-right' } as ControlOptions,
+    },
+    {
+      controlName: 'TrafficControl',
+      controlOptions: { incidents: true },
+      options: { position: 'top-left' } as ControlOptions,
+    },
+    {
+      controlName: 'TrafficLegendControl',
+      controlOptions: {},
+      options: { position: 'bottom-left' } as ControlOptions,
+    },
+  ];
 
 const MapComponent: React.FC = () => (
     <div className="w-100 h-100">
-        <AzureMap options={option}>
+        <AzureMap options={option} controls={controls}>
         </AzureMap>
     </div>
 )
